e1b8fc866a5ebdd37ab0f1488235e1cd39901b62
lakatoshv <some.pochta1@gmail.com>
2018-07-02 22:47:19 +0300

    Стилізована форма авторизації

--- application/modules/default/forms/Login.php
+++ application/modules/default/forms/Login.php
@@ -4,22 +4,13 @@ class Form_Login extends Zend_Form
 {
     public function init()
     {
-        $username = $this->createElement('text','email');
-        $username->setLabel('Емейл: *')
-                ->setRequired(true);
-                
-        $password = $this->createElement('password','password');
-        $password->setLabel('Пароль: *')
-                ->setRequired(true);
-                
-        $signin = $this->createElement('submit','signin');
-        $signin->setLabel('Ввійти')
-                ->setIgnore(true);
-                
-        $this->addElements(array(
-                        $username,
-                        $password,
-                        $signin,
+        $this->addElement('password', 'password', array(
+            'label' => "Пароль:", 'class' => "form-control", "id" => "inputPassword"
         ));
+
+        $this->addElement('text', 'email', array(
+            'label' => "Електронна пошта:", 'class' => "form-control", "id" => "inputEmail"
+        ));
+
     }
 }
