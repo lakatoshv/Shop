4c71f919b6a227e3990151e93cab0f1aedb29c20
lakatoshv <some.pochta1@gmail.com>
2018-07-15 23:57:43 +0300

    Доданий блок Відгуки в адмінку

---
+++ application/modules/admin/controllers/ReviewsController.php
@@ -0,0 +1,84 @@
+<?php
+
+class Admin_ReviewsController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+    {
+        $reviewsTbl = new Shop_Model_DbTable_Reviews();
+        /*
+        $show = Zend_Controller_Request_Http::getCookie("show", 5);
+        $paginator = new Zend_Paginator(new Zend_Paginator_Adapter_DbSelect($newsTbl->fetchAll()));
+        $paginator->setItemCountPerPage($show)->setCurrentPageNumber($page);
+        $this->view->news = $paginator;
+        */
+        $this->view->reviews = $reviewsTbl->SelectGroupedReviews();
+        //$this->view->show = $show;
+    }
+
+    public function addAction()
+    {
+        $newsTbl = new Admin_Model_DbTable_News();
+        $form = new Admin_Form_AddNews();
+        $this->view->form=$form;
+        if($this->getRequest()->isPost()){
+            if($form->isValid($_POST)){
+                $data = $form->getValues();
+                $date = new Zend_Date();
+                $data["date"] = "{$date->toString('YYYY-MM-dd HH:mm:ss')}";
+                $newsTbl->insert($data);
+                
+            }
+        }
+    }
+
+    public function editAction()
+    {
+
+        $newsTbl = new Admin_Model_DbTable_News();
+        $news_id = $this->_getParam("news", null);
+        $newsTable = new Shop_Model_DbTable_News();
+        $this->view->news = $newsTable->showNews($news_id);
+
+        $form = new Admin_Form_EditNews();
+
+        $this->view->form=$form;
+        if($this->getRequest()->isPost()){
+            if($form->isValid($_POST)){
+                $data = $form->getValues();
+                $date = new Zend_Date();
+                $data["date"] = "{$date->toString('YYYY-MM-dd HH:mm:ss')}";
+                $newsTbl->updateNews($data, $news_id);
+            }
+        }
+    }
+
+    public function deleteAction()
+    {
+        
+        $newsTbl = new Admin_Model_DbTable_News();
+        $news_id = $this->_getParam("news", null);
+        $newsTbl->deleteNews($news_id);
+        $this->_redirect('shop/products/list');
+    }
+    public function showAction(){
+        $newsTbl = new Admin_Model_DbTable_News();
+        $news_id = $this->_getParam("news", null);
+        $newsTable = new Shop_Model_DbTable_News();
+        $this->view->news = $newsTable->showNews($news_id);
+    }
+
+
+}
+
+
+
+
+
+
+
--- application/modules/shop/models/DbTable/Reviews.php
+++ application/modules/shop/models/DbTable/Reviews.php
@@ -5,6 +5,21 @@
 class Shop_Model_DbTable_Reviews extends Zend_Db_Table_Abstract
 {
 	protected $_name = "reviews";
+    public function SelectGroupedReviews(){
+        $select = $this->_db->select()
+                 ->from(array('reviews', 'products'),
+                    array('reviews.product_id', 'reviews.COUNT(product_id) as `count`'))->group('reviews.product_id');
+                 //->from(array('p' => 'products'),
+                    //array('p.product_id', 'p.product_name'));
+                 //SELECT reviews.`product_id`, COUNT(reviews.`product_id`), products.`id`, products.`name` FROM `reviews` as reviews, `products` as products where products.`id` = reviews.`product_id` GROUP BY `product_id`
+                 //->order('type');
+                 //->having('count(*)= 1');
+        $result = $this->getAdapter()->fetchAll($select);
+        if($result){
+            return $result;
+        }
+        return false;
+    }
 	function getReviews($type, $value)
     {   
         $select = $this->_db->select()
