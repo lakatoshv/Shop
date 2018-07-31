e427149ee63b383bad914884e446f27638dca75f
lakatoshv <some.pochta1@gmail.com>
2018-07-31 23:27:09 +0300

    Додана можливість переглянути замовлення у блоці замовлень

--- application/modules/default/controllers/OrdersController.php
+++ application/modules/default/controllers/OrdersController.php
@@ -19,7 +19,42 @@ class OrdersController extends Zend_Controller_Action
 
     public function showAction()
     {
+        $cartsTbl = new Shop_Model_DbTable_Cart();
+        $carts = $cartsTbl->getCart("cart_ip", "{$_SERVER['REMOTE_ADDR']}");
+        $count = 0;
+        $sum = 0;
+        $products = array();
+        $images = array();
 
+        $productsTbl = new Shop_Model_DbTable_Products();
+        $imagesTbl = new Shop_Model_DbTable_UploadImages();
+
+        if(count($carts) > 1){
+          foreach ($carts as $cart) {
+            $sum += $cart["cart_price"] * $cart["cart_count"];
+            $count += $cart["cart_count"];
+            $products[] = $productsTbl->showProduct($cart["cart_id_products"]);
+            $images[] = $imagesTbl->getImages($cart["cart_id_products"]);
+          }
+        }
+        else{
+          $sum += $carts[0]["cart_price"] * $carts[0]["cart_count"];
+          $count += $carts[0]["cart_count"];
+          $products[] = $productsTbl->showProduct($carts[0]["cart_id_products"]);
+          $images[] = $imagesTbl->getImages($carts[0]["cart_id_products"]);
+        }
+        $this->view->carts = $carts;
+        $this->view->sum = $sum;
+        $this->view->count = $count;
+        $this->view->products = $products;
+        $this->view->images = $images;
+
+        if(filter_input(INPUT_COOKIE, 'customer_data')){
+            $data = unserialize(filter_input(INPUT_COOKIE, 'customer_data'));
+            $this->view->customer_data = $data;
+            $date = new Zend_Date();
+            $data["datetime"] = "{$date->toString('YYYY-MM-dd HH:mm:ss')}";
+        }
     }
 
     public function cancelAction()
