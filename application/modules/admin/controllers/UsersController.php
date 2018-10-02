3260d5dfd1e1f5811ffabcdaba4b7380a546ca44
lakatoshv <some.pochta1@gmail.com>
2018-10-02 21:23:35 +0300

    Доданий блок користувачів в адмінці

--- application/modules/admin/controllers/UsersController.php
+++ application/modules/admin/controllers/UsersController.php
@@ -7,7 +7,17 @@ class Admin_UsersController extends Zend_Controller_Action
 	
 	public function init(){}
 
-	public function indexAction(){}
+	public function indexAction(){
+		$usersTBl = new Model_DbTable_Users();
+        /*
+        $show = Zend_Controller_Request_Http::getCookie("show", 5);
+        $paginator = new Zend_Paginator(new Zend_Paginator_Adapter_DbSelect($newsTbl->fetchAll()));
+        $paginator->setItemCountPerPage($show)->setCurrentPageNumber($page);
+        $this->view->news = $paginator;
+        */
+        $this->view->users = $usersTBl->selectAll();
+        //$this->view->show = $show;
+	}
 
-	public function listAction(){}
+	public function showAction(){}
 }
--- application/modules/default/models/DbTable/Users.php
+++ application/modules/default/models/DbTable/Users.php
@@ -1,6 +1,6 @@
 <?php
 
-class Model_DbTable_Users extends Zend_Db_Table_Abstract
+class Model_DbTable_customers extends Zend_Db_Table_Abstract
 {
 
     protected $_name = 'customers';
@@ -14,6 +14,43 @@ class Model_DbTable_Users extends Zend_Db_Table_Abstract
             return true;
         }
         return false;
+    }
+    function getcustomers($type, $value)
+    {   
+        $select = $this->_db->select()
+                 ->from('customers')
+                 ->where("$type=?",$value);
+                 //->order('type');
+                 //->having('count(*)= 1');
+$result = $this->getAdapter()->fetchAll($select);
+        if($result){
+            return $result;
+        }
+        return false;
+    }
+    function selectAll()
+    {   
+        $select = $this->_db->select()
+                 ->from('customers');
+                 //->order('type');
+                 //->having('count(*)= 1');
+        $result = $this->getAdapter()->fetchAll($select);
+        if($result){
+            return $result;
+        }
+        return false;
+    }
+    public function insertOrder($data){
+        //INSERT INTO table_name(*) VALUES (*);
+        $this->_db->insert('customers', $data);
+    }
+    public function updateOrder($data, $id){
+        //UPDATE table_name SET condition WHERE condition
+        $this->_db->update('customers', $data, 'id = '.$id);
+    }
+    public function deleteOrder($type, $value){
+        //DELETE FROM table_name WHERE condition;
+        $this->_db->delete('customers', "`$type` = '".$value."'");
     }
 
 }
